619025d2606de317b730051bbc95f87e0d341d57
tsravani <tsravani@CSCINDAE750334.asiapac.globalcsc.net>
2017-02-01 18:41:19 +0530

    Second Commit

--- FirstPJT/src/pkg1/Example.java
+++ FirstPJT/src/pkg1/Example.java
@@ -5,6 +5,9 @@ public class Example {
 	public static void main(String[] args) {
 		
 			System.out.println("Sravani");
+			
+			//change for second commit
+			System.out.println("Commit 2");
 	}
 
 }
